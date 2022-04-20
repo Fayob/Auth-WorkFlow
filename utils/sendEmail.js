d4feb68b970cadeab48c90f64f0e039b8bf5ac1f
= <adedejifavour6@gmail.com>
2022-04-20 10:11:07 +0400

    sendEmail Refactor

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,17 +5,15 @@ const formData = require("form-data");
 const API_KEY = "056dce4ed786644d0da165d71f0cff5b-38029a9d-fc459c3c";
 const DOMAIN = "sandbox2671e686a5a7473da65e0ddd9a62f0e0.mailgun.org";
 
-const sendEmail = async () => {
+const sendEmail = async ({ to, subject, html }) => {
   const mailgun = new Mailgun(formData);
   const client = mailgun.client({ username: "Fayob", key: API_KEY });
 
   const messageData = {
     from: "FAYOB <adedejifavour6@gmail.com>",
-    to: "alexwillyreal@gmail.com, lynnvivian78@gmail.com",
-    subject: "Testing Email With Nodejs",
-    text: "I just want to Confirm if it'll send to two or more email address",
-    html:
-      "<h1/>I just want to <strong>Confirm</strong>  if it'll send to two or more email address</h1>",
+    to,
+    subject,
+    html,
   };
   try {
     client.messages.create(DOMAIN, messageData);
