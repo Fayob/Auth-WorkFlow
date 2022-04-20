0154ac4811f98f9acf824468fc47e46b9a6c9709
= <adedejifavour6@gmail.com>
2022-04-20 10:04:57 +0400

    model modified

---
+++ models/productModel.js
@@ -0,0 +1,86 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Please input product name"],
+      minlength: [2, "Please Input at least 2 letters"],
+      maxlength: [50, "Please Input at most 50 letters"],
+    },
+    price: {
+      type: Number,
+      //   required: true,
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: [true, "Please describe your product"],
+      minlength: [50, "Please Input at least 50 characters"],
+    },
+    image: {
+      type: String,
+      default: "/uploads/example.jpg",
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: ["office", "kitchen", "bedroom"],
+    },
+    company: {
+      type: String,
+      required: [true, "Please input company name"],
+      enum: {
+        values: ["ikea", "liddy", "marcos"],
+        message: "{VALUE} is not supported",
+      },
+    },
+    colors: {
+      type: [String],
+      required: true,
+      default: ["#222"],
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    freeShipping: {
+      type: Boolean,
+      default: false,
+    },
+    inventory: {
+      type: Number,
+      required: true,
+      default: 15,
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+    },
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+ProductSchema.virtual("reviews", {
+  ref: "Review",
+  localField: "_id",
+  foreignField: "product",
+  justOne: false,
+  match: { rating: 5 },
+});
+
+ProductSchema.pre("remove", async function (next) {
+  await this.model("Review").deleteMany({ product: this._id });
+});
+
+module.exports = mongoose.model("Product", ProductSchema);
