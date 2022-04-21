8077a688f5b2db747a26588a5931a23ed3cac8e7
= <adedejifavour6@gmail.com>
2022-04-21 20:22:31 +0400

    verification and reset password functionality added

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,26 +1,45 @@
 const jwt = require("jsonwebtoken");
 
 const createJWT = ({ payload }) => {
-  const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_LIFETIME,
-  });
+  const token = jwt.sign(payload, process.env.JWT_SECRET);
   return token;
 };
 
-const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
+const isTokenValid = (token) => jwt.verify(token, process.env.JWT_SECRET);
 
-const attachCookiesToResponse = ({ res, user }) => {
-  const token = createJWT({ payload: user });
+const attachCookiesToResponse = ({ res, user, refreshToken }) => {
+  const accessTokenJWT = createJWT({ payload: { user } });
+  const refreshTokenJWT = createJWT({ payload: { user, refreshToken } });
 
   const threeHours = 1000 * 60 * 60 * 3;
+  const thirtyDays = 1000 * 60 * 60 * 24 * 30;
 
-  res.cookie("token", token, {
+  res.cookie("accessToken", accessTokenJWT, {
     httpOnly: true,
-    Expires: threeHours,
     secure: process.env.NODE_ENV === "production",
     signed: true,
+    expires: new Date(Date.now() + threeHours),
+  });
+
+  res.cookie("refreshToken", refreshTokenJWT, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    signed: true,
+    expires: new Date(Date.now() + thirtyDays),
   });
 };
+// const attachSingleCookiesToResponse = ({ res, user }) => {
+//   const token = createJWT({ payload: user });
+
+//   const threeHours = 1000 * 60 * 60 * 3;
+
+//   res.cookie("token", token, {
+//     httpOnly: true,
+//     expires: threeHours,
+//     secure: process.env.NODE_ENV === "production",
+//     signed: true,
+//   });
+// };
 
 module.exports = {
   createJWT,
